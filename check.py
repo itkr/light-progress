eaa58d9ead35fb7f59e2f42ec3a16fab615ccebe
itkr <1531860+itkr@users.noreply.github.com>
2018-07-07 21:55:51 +0900

    Update test

--- check.py
+++ check.py
@@ -5,7 +5,7 @@ from time import sleep
 from progress_bar.commandline import CommandLineProgressBar, widget
 
 
-def main():
+def test_widget():
     widgets = [widget.Bar(under=' '), widget.Percentage(), widget.Num()]
     format_str = '{} {} ({})'
     CommandLineProgressBar.iteration(
@@ -14,11 +14,30 @@ def main():
         widgets=widgets,
         format_str=format_str)
 
+
+def test_default():
     with CommandLineProgressBar(100) as progress_bar:
-        for _item in range(100):
+        for item in range(100):
             sleep(0.01)
             progress_bar.forward()
 
+
+def test_error():
+    try:
+        with CommandLineProgressBar(100) as progress_bar:
+            for item in range(100):
+                sleep(0.01)
+                if item >= 70:
+                    raise Exception('test error')
+                progress_bar.forward()
+    except Exception as e:
+        print(e.message)
+
+
+def main():
+    test_widget()
+    test_default()
+    test_error()
 
 if __name__ == '__main__':
     main()
